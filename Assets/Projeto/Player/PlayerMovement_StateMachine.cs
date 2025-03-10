9b49bc239f272cd33077d1059b9b2d364f23ef2b
Gabriel Neves <60232800+gnpq1996@users.noreply.github.com>
2025-03-10 10:29:22 -0300

    state eventso implementados com sucesso

--- Assets/Projeto/Player/PlayerMovement_StateMachine.cs
+++ Assets/Projeto/Player/PlayerMovement_StateMachine.cs
@@ -1,17 +1,20 @@
-using Demo.InputManagers;
-using Unity.Cinemachine;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Events;
+using DG.Tweening;
 
-namespace Demo.StateMachines
+namespace Demo.StateMachines.Player
 {
     public class PlayerMovement_StateMachine : StateMachine
     {
+        #region Variables
         public State<PlayerMovement_StateMachine> previousState;
         public State<PlayerMovement_StateMachine> currentState;
+
         [Header("Components")]
         [field: SerializeField] public Demo.InputManagers.PlayerMovement_InputManager inputManager { get; private set; }
         [field: SerializeField] public Rigidbody2D rb { get; private set; }
+        [field: SerializeField] public Transform gfx { get; private set; }
+
 
         [System.Serializable]
         public class Movement_Info
@@ -32,13 +35,15 @@ namespace Demo.StateMachines
         }
         public Gravity_Info gravity;
 
+
         [System.Serializable]
         public class Jump_Info
         {
             [field: SerializeField] public float jumpForce { get; private set; } = 10;
-            [field: SerializeField, Range(0,1)] public float jumpStopForce { get; private set; } = 0.5f;
+            [field: SerializeField, Range(0, 1)] public float jumpStopForce { get; private set; } = 0.5f;
         }
         public Jump_Info jumpInfo;
+
 
         [System.Serializable]
         public class GroundCheck
@@ -66,6 +71,18 @@ namespace Demo.StateMachines
             }
         }
         public GroundCheck groundCheck;
+
+
+        [System.Serializable]
+        public class State_Events
+        {
+            [field: SerializeField] public UnityEvent onJump { get; private set; }
+            [field: SerializeField] public UnityEvent onAirJump { get; private set; }
+        }
+        public State_Events stateEvents;
+        #endregion
+
+        #region MonoBehaviours
         private void Awake()
         {
             if (rb == null) rb = GetComponent<Rigidbody2D>();
@@ -79,6 +96,12 @@ namespace Demo.StateMachines
             Tick_Machine();
         }
 
+
+
+        #endregion
+
+        #region StateMachine
+        //====================================================================================================================
         public virtual void Tick_Machine()
         {
             if (currentState != null) currentState.Tick_State(this);
@@ -93,8 +116,33 @@ namespace Demo.StateMachines
             }
             previousState = currentState;
             currentState = newState;
+
+            EventHandler_StateEvents();
         }
 
+        void EventHandler_StateEvents()
+        {
+            switch (currentState)
+            {
+                case Demo.StateMachines.Player.Jumping_State state:
+                    {
+                        stateEvents.onJump?.Invoke();
+                        break;
+                    }
+                case Demo.StateMachines.Player.Air_Jump_State state:
+                    {
+                        stateEvents.onAirJump?.Invoke();
+                        break;
+                    }
+            }
+        }
+
+
+
+        #endregion
+
+        #region State Tools
+        //====================================================================================================================
         public void Movement_Free()
         {
             if (rb.linearVelocityY < -0.01f)
@@ -114,11 +162,40 @@ namespace Demo.StateMachines
             rb.AddForceX(finalVelocity);
         }
 
+        public void Jump()
+        {
+            gfx.DOShakeScale(0.2f, 0.3f, 10, 90, false);
+            rb.AddForce(Vector2.up * jumpInfo.jumpForce, ForceMode2D.Impulse);
+        }
+
+        public void AirJump()
+        {
+            rb.linearVelocityY = 0;
+            rb.AddForce(Vector2.up * jumpInfo.jumpForce * 0.6f, ForceMode2D.Impulse);
+        }
+        public void StopJumpin()
+        {
+            if (rb.linearVelocityY > 0.1f)
+            {
+                Debug.Log("Reduzing jump speed");
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocityY * jumpInfo.jumpStopForce);
+            }
+        }
+
+
+
+
+        #endregion
+
+        #region Gizmos
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.red;
             Gizmos.DrawCube(groundCheck.transform.position, new Vector2(groundCheck.checkRadius, groundCheck.checkHeight));
         }
+
+
+        #endregion
     }
 
     public class Idle_State : State<PlayerMovement_StateMachine>
@@ -131,34 +208,17 @@ namespace Demo.StateMachines
         public override void Tick_State(PlayerMovement_StateMachine stateMachine)
         {
             Debug.Log("Tick: Idle");
+            var trigger_jump = stateMachine.inputManager.trigger_jump;
+
             stateMachine.Movement_Free();
 
-            if (stateMachine.inputManager.trigger_jump.trigger)
+            if (trigger_jump.trigger)
             {
-                //if (stateMachine.groundCheck.isGrounded ||
-                //    stateMachine.inputManager.trigger_jump.graceTime_coyoteTime
-                //    +
-                //    stateMachine.inputManager.trigger_jump.timeStamp
-                //    >
-                //    Time.time)
-                //{
-                //    stateMachine.inputManager.trigger_jump.Deactivate();
-                //    stateMachine.Set_State(new Jumping_State(stateMachine));
-                //}
-                if (stateMachine.groundCheck.isGrounded)
+                if (stateMachine.groundCheck.isGrounded
+                    ||
+                    trigger_jump.graceTime_coyoteTime + trigger_jump.timeStamp > Time.time)
                 {
-                    Debug.Log(ccolor.orange("Grounded jump"));
-                    stateMachine.inputManager.trigger_jump.Deactivate();
-                    stateMachine.Set_State(new Jumping_State(stateMachine));
-                }
-                else if (stateMachine.inputManager.trigger_jump.graceTime_coyoteTime
-                    +
-                    stateMachine.groundCheck.timeStamp_lastIsGrounded
-                    >
-                    Time.time)
-                {
-                    Debug.Log(ccolor.orange("Coyote time jump"));
-                    stateMachine.inputManager.trigger_jump.Deactivate();
+                    trigger_jump.Deactivate();
                     stateMachine.Set_State(new Jumping_State(stateMachine));
                 }
             }
@@ -174,24 +234,28 @@ namespace Demo.StateMachines
         {
             Debug.Log(ccolor.cyan("Changing State: Jump"));
             timeStamp = Time.time + delay;
-            stateMachine.rb.AddForce(Vector2.up * stateMachine.jumpInfo.jumpForce, ForceMode2D.Impulse);
+            stateMachine.Jump();
         }
 
         public override void Tick_State(PlayerMovement_StateMachine stateMachine)
         {
             Debug.Log("Tick: Jump");
+            var trigger_stopJump = stateMachine.inputManager.trigger_stopJumping;
+            var trigger_jump = stateMachine.inputManager.trigger_jump;
+
             stateMachine.Movement_Free();
 
             if (!stateMachine.groundCheck.isGrounded)
             {
-                if (stateMachine.inputManager.trigger_stopJumping.trigger)
+                if (trigger_stopJump.trigger)
                 {
-                    stateMachine.inputManager.trigger_stopJumping.Deactivate();
-                    if (stateMachine.rb.linearVelocityY > 0.1f)
-                    {
-                        Debug.Log("Reduzing jump speed");
-                        stateMachine.rb.linearVelocity = new Vector2(stateMachine.rb.linearVelocity.x, stateMachine.rb.linearVelocityY * stateMachine.jumpInfo.jumpStopForce);
-                    }
+                    trigger_stopJump.Deactivate();
+                    stateMachine.StopJumpin();
+                }
+                else if (trigger_jump.trigger)
+                {
+                    trigger_jump.Deactivate();
+                    stateMachine.Set_State(new Air_Jump_State(stateMachine));
                 }
                 return;
             }
@@ -202,4 +266,37 @@ namespace Demo.StateMachines
 
         }
     }
+
+    public class Air_Jump_State : State<PlayerMovement_StateMachine>
+    {
+        public float timeStamp = 0;
+
+        public Air_Jump_State(PlayerMovement_StateMachine stateMachine) : base(stateMachine)
+        {
+            Debug.Log(ccolor.cyan("Changing State: Air Jump"));
+            timeStamp = Time.time;
+            stateMachine.AirJump();
+        }
+        public override void Tick_State(PlayerMovement_StateMachine stateMachine)
+        {
+            Debug.Log("Tick: Air Jump");
+            var trigger_stopJump = stateMachine.inputManager.trigger_stopJumping;
+
+            stateMachine.Movement_Free();
+
+            if (!stateMachine.groundCheck.isGrounded)
+            {
+                if (trigger_stopJump.trigger)
+                {
+                    trigger_stopJump.Deactivate();
+                    stateMachine.StopJumpin();
+                }
+                return;
+            }
+
+            stateMachine.Set_State(new Idle_State(stateMachine));
+
+        }
+    }
+
 }
